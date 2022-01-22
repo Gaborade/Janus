0b3a8ec1d9d811dccbabf659fa70e1f530b5bf10
Gaborade <gabdanq@gmail.com>
2022-01-22 13:00:30 +0000

    refactored cli.py

--- cli.py
+++ cli.py
@@ -1,21 +1,8 @@
 import socket
 import sys
 
-class CLIException(Exception):
-    pass
 
 DATABASE_COMMANDS = ("insert", "retrieve", "update", "delete")
-
-"""HOST, PORT = "localhost", 4000
-data = " ".join(sys.argv[1:])
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
-    received = str(sock.recv(1024), "utf-8")
-
-print(f'{received}')
-"""
 
 def commands_cli():
     global DATABASE_COMMANDS
