0fb9b798db1c2ce264b2efe6051ef1526efa4aab
Gaborade <gabdanq@gmail.com>
2022-01-22 18:36:15 +0000

    added log description for server process id/ temporary fix of socket re-connection because of socket context manager

--- cli.py
+++ cli.py
@@ -1,34 +1,36 @@
 import socket
 import sys
 
-
 DATABASE_COMMANDS = ("insert", "retrieve", "update", "delete")
+SYSTEM_EXITS = ("exit", "quit", "q", "Q", "QUIT", "Quit")
 
 def commands_cli():
     global DATABASE_COMMANDS
+    global SYSTEM_EXITS
+
     HOST, PORT = "localhost", 4000
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        try:
-            sock.connect((HOST, PORT))
-        except ConnectionRefusedError:
-            raise ConnectionRefusedError("Cannot connect to host server, perhaps start key_db_server")
 
-        while True:
-            command = input("key_db_cli $  ").strip()
-            if command == "quit" or command == "exit":
-                sys.exit(0)
+    while True:
+        command = input("key_db_cli $  ").strip()
+        if command in SYSTEM_EXITS:
+            sys.exit()
+        else:
+            command_split = command.split()
+            command_len = len(command_split)
+            if command_len < 2:
+                print("Few arguments given", file=sys.stderr)
+            elif command_len > 3:
+                print("Too many arguments", file=sys.stderr)
+
+            if command_split[0] not in DATABASE_COMMANDS:
+                print("Command not supported", file=sys.stderr)
             else:
-                command_split = command.split(" ")
-                command_len = len(command_split)
-                if command_len < 2:
-                    print("Few arguments given", file=sys.stderr)
-                    continue
-                elif command_len > 3:
-                    print("Too many arguments", file=sys.stderr)
-
-                if command_split[0] not in DATABASE_COMMANDS:
-                    print("Command not supported", file=sys.stderr)
-                else:
+                # socket is re-initialised everytime
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    try:
+                        sock.connect((HOST, PORT))
+                    except ConnectionRefusedError:
+                        raise ConnectionRefusedError("Cannot connect to host server, perhaps start db server")
                     sock.sendall(bytes(command + "\n", "utf-8"))
                     response = str(sock.recv(1024), "utf-8")
                     print(response)
--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import socketserver
 import sys
 from log import logger
 from keys import Key
-
+import os
 
 class TCPServerHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
         db = Key()
         self.data = self.rfile.readline().strip().decode()
-        command_split = self.data.split(" ")
+        command_split = self.data.split()
 
         if len(command_split) == 3:
             db_command, key, value = command_split
@@ -23,7 +23,7 @@ class TCPServerHandler(socketserver.StreamRequestHandler):
             command = getattr(db, f"{db_command}")
             if command_len == 3:
                 command(key, value)
-                self.wfile.write(f"{key} set".encode())
+                self.wfile.write("0".encode())
             elif command_len == 2:
                 get_key = command(key)
                 self.wfile.write(f"{get_key}".encode())
@@ -34,8 +34,12 @@ if __name__ == "__main__":
 
     with socketserver.TCPServer((HOST, PORT), TCPServerHandler) as server:
         try:
-            logger.info(f"KEY VALUE DB SERVER STARTING, {HOST}:{PORT}")
+            process_id = os.getpid()
+            logger.info(f"Starting process id {process_id}")
+            logger.info(f"Key value db server starting, address={HOST}:{PORT}")
             server.serve_forever()
+
         except BaseException:
-            logger.info(f"KEY VALUE DB SERVER SHUTTING DOWN")
-            sys.exit(0)
+            logger.info(f"Closing process id {process_id}")
+            logger.info(f"Key value db server shutting down")
+            sys.exit()
